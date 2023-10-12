cec4adc3894c2d0a3a43cb613ca066c58442bae7
Team Member's Name <team.member@example.com>
2023-10-12 08:08:08 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 my_array=("apple","banana","cherry")
-echo "first fruits are $my_array[0]"
-echo "first fruits are $my_array[1]"
-echo "first fruits are $my_array[2]"
+echo "first fruits are ${my_array[0]}"
+echo "first fruits are ${my_array[1]}"
+echo "first fruits are ${my_array[2]}"
