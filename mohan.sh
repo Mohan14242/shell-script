35da3e8e08bdc9e7bf818679092a57628cdc0a5b
Team Member's Name <team.member@example.com>
2023-10-12 08:10:40 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-my_array=("apple" "banana" "cherry")
-echo "first fruit is ${my_array[0]}"
-echo "second fruit is ${my_array[1]}"
-echo "third fruit is ${my_array[2]}"
+date='date'
+echo "today date is $date"
