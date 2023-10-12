ce90aa2e59fef421313dede857b47346c1c1816d
Team Member's Name <team.member@example.com>
2023-10-12 08:07:23 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "please enter your friend name mohan"
-read  n1
-echo "please enter your name mohan"
-read  n2
-echo "my name is $n1"
-echo "my firend name is $n2"
+my_array=("apple","banana","cherry")
+echo "first fruits are $my_array[0]"
+echo "first fruits are $my_array[1]"
+echo "first fruits are $my_array[2]"
