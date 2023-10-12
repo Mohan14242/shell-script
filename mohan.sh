8df54f41c25e51859908acb821aa3afc427ba164
Team Member's Name <team.member@example.com>
2023-10-12 08:05:18 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "please enter your friend name mohan"
-read -p n1
+read  n1
 echo "please enter your name mohan"
-read -p n2
+read  n2
 echo "my name is $n1"
 echo "my firend name is $n2"
