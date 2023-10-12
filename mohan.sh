919c40b0c28d32fbd1b9602dc383f6ddc1f9f116
Team Member's Name <team.member@example.com>
2023-10-12 08:01:14 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-date=$(date)
-echo "todat date id $date"
-
+name1=$1
+name2=$2
+echo " hi $name1 how are  you this is $name2" 
+echo " hi $name2 i am fine what about you "
+echo " fine where are you $name1" 
+echo "i am in the home "
