ff6e7e1ad96a417e6633fa82e381a85019d9d16c
Team Member's Name <team.member@example.com>
2023-10-12 20:52:33 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -2,3 +2,11 @@
 
 name="mohan"
 echo "this is the $name"
+echo " the script name is $0"
+script_name=$0
+echo "the scripot  name is $0"
+if [ "$EUID" -eq 0]; then 
+    echo "the current user is root usre"
+else
+    echo "not runnning as the root user"
+fi
