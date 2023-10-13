ad658a098f32d2d6c772a6c0008b43bf8e0c0a06
Team Member's Name <team.member@example.com>
2023-10-13 20:17:03 +0530

    mohan

--- mohan.sh
+++ mohan.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-name="mohan"
-echo "this is the $name"
-echo " the script name is $0"
-script_name=$0
-echo "the scripot  name is $0"
-if [ "$EUID" -eq 0]; then 
-    echo "the current user is root usre"
-else
-    echo "not runnning as the root user"
-fi
-
-echo "usage $0[options]"
-log_file='mohan.log'
-echo "$(date) -script $0 executed">>"$log_file"
+number=$1
+if [ $number -gt 10]
+then 
+    echo "numebr is greater than 10"
+else    
+    echo "the number is less than 10"
