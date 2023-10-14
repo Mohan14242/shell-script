2d882a02c64d7bb39268936b01d21c26fe70af2c
Team Member's Name <team.member@example.com>
2023-10-14 20:15:36 +0530

    shell script

--- mohan.sh
+++ mohan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo " the script name is $#"
-if [ $# -eq 2]; then 
+if [ $# -eq 2 ]; then 
     echo "yes yu have provided the correct number of arguments"
 else
     echo " you have not provided the enough arguments"
