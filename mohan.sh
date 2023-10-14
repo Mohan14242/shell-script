47d8e38529df2d86c49c6cf5451c89b53548cfb8
Team Member's Name <team.member@example.com>
2023-10-14 20:33:40 +0530

    shell script

--- mohan.sh
+++ mohan.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-mkdir "$1"
-echo "dreictory created"
+case $1 in 
+    "start")
+        echo "starting the service"
+        ;;
+    "stop")
+        echo "stopping the sevice"
+        ;;
+    *) 
+        echo "unkone comand means"
+        ;;
+esac
